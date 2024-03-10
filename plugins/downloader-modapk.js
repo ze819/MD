e0b8db71b7183f0c0ef396c8062e0ca6b7a34b8e
𝑴𝒐𝒏𝒌𝒆𝒚-𝑫-𝑳𝒐𝒇𝒇𝒚١⁵ <155516056+ze819@users.noreply.github.com>
2024-03-11 01:16:16 +0300

    Update downloader-modapk.js

--- plugins/downloader-modapk.js
+++ plugins/downloader-modapk.js
@@ -1,18 +1,32 @@
-import {search, download} from 'aptoide-scraper';
-const handler = async (m, {conn, usedPrefix: prefix, command, text}) => {
- if (!text) throw `*[❗] Ingrese el nombre de la APK que quiera buscar.*`;
-  try {    
-    const searchA = await search(text);
-    const data5 = await download(searchA[0].id);
-    let response = `📲 *Descargador de Aptoide* 📲\n\n📌 *Nombre:* ${data5.name}\n📦 *Package:* ${data5.package}\n🕒 *Última actualización:* ${data5.lastup}\n📥 *Tamaño:* ${data5.size}`
-    await conn.sendMessage(m.chat, {image: {url: data5.icon}, caption: response}, {quoted: m});
- if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
-      return await conn.sendMessage(m.chat, {text: '*[ ⛔ ] El archivo es demasiado pesado por lo que no se enviará.*'}, {quoted: m});
-    }
-    await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: m});
-  } catch {
-    throw `*[❗] Error, no se encontrarón resultados para su búsqueda.*`;
-  }    
-};
-handler.command = /^(apkmod|modapk|dapk2|aptoide|aptoidedl)$/i;
-export default handler;
+const rewards = {
+  exp: 40000,
+  money: 55555,
+  potion: 10,
+  mythic: 3,
+  legendary: 1
+}
+
+const cooldown = 2592000000
+let handler = async (m) => {
+  let user = global.db.data.users[m.sender]
+  if (new Date - user.lastmonthly < cooldown) throw `تم تجميع المكافئه الشهريه بالفعل *${((user.lastmonthly + cooldown) - new Date()).toTimeString()}*`
+  let text = ''
+  for (let reward of Object.keys(rewards)) if (reward in user) {
+      user[reward] += rewards[reward]
+      text += `*+${rewards[reward]}* ${reward}\n`
+  }
+  //conn.sendButton(m.chat,'*––––––『 MONTHLY 』––––––*', text.trim(), null, [['Inventory', '.inv'], ['Menu', '.menu']],m)
+  m.reply(`
+  🎁*المكافئه الشهريه*🎁
+
+  ▢*تم الحصول على:*
+   ${text}`)
+  user.lastmonthly = new Date * 1
+}
+handler.help = ['monthly']
+handler.tags = ['rpg']
+handler.command = /^(شهري)$/i
+
+handler.cooldown = cooldown
+
+export default handler
