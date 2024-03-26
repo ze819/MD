edd64d6dd4efa8c991d348b9583d4ec1037be015
𝑴𝒐𝒏𝒌𝒆𝒚-𝑫-𝑳𝒐𝒇𝒇𝒚١⁵ <155516056+ze819@users.noreply.github.com>
2024-03-26 06:33:33 +0300

    Update afk-_afk.js

--- plugins/afk-_afk.js
+++ plugins/afk-_afk.js
@@ -1,14 +1,15 @@
 let handler = async (m, { conn }) => {
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
     let name = conn.getName(who)
     let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './Guru.jpg')
     conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/overlay/wasted', {
       avatar: pp, 
-    }), 'waste.png', `*Ah! Shit :* ${name}\n\nWastedeeznuts`, m)
+    }), 'waste.png', `*تم يا ${name}*\n\n*𝐋𝐔𝐅𝐅𝐘-𝐁𝐎𝐓*`,fkontak)
   }
   
   handler.help = ['waste @user']
   handler.tags = ['fun']
-  handler.command = ['بروفايل'] 
+  handler.command = ['1بروفايل'] 
   
   export default handler
