6674d065b6b287c9405c34c336bbb0a70dccb1f4
𝑴𝒐𝒏𝒌𝒆𝒚-𝑫-𝑳𝒐𝒇𝒇𝒚١⁵ <155516056+ze819@users.noreply.github.com>
2024-03-26 10:48:37 +0300

    Update afk-_afk.js

--- plugins/afk-_afk.js
+++ plugins/afk-_afk.js
@@ -11,7 +11,7 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
 		if (!text) throw tee;
 		let chut = `https://api.caliph.biz.id/api/kaneki?nama=${encodeURIComponent(text)}&apikey=caliphkey`;
 		conn.sendFile(m.chat, chut, 'logo.png', `✅ Result`, m);
-		m.react(done);
+		(done);
 		break;
 		
 	  case 'gfx2': 
@@ -21,27 +21,27 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
 		let loda = `https://api.caliph.biz.id/api/girlneko?nama=${encodeURIComponent(a.trim())}&nama2=${encodeURIComponent(b.trim())}&apikey=caliphkey`;
 		conn.sendFile(m.chat, loda, 'logo.png', `✅ Result`, m);
 		
-		m.react(done);
+		(done);
 		break;
 		
 	  case 'gfx3':
 		if (!text) throw tee;
 		let cp = `https://api.caliph.biz.id/api/rem?nama=${encodeURIComponent(text.trim())}&apikey=caliphkey`;
 		conn.sendFile(m.chat, cp, 'logo.png', `✅ Result`, m);
-		m.react(done);
+		(done);
 		break;
 		
 	  case 'gfx4': 
 		if (!text) throw tee;
 		let gandu = `https://api.caliph.biz.id/api/textpro/matrix?text=${encodeURIComponent(text)}&apikey=caliphkey`;
 		conn.sendFile(m.chat, gandu, 'logo.png', `✅ Result`, m);
-		m.react(done);
+		(done);
 		break;
 		case 'gfx5':
     if (!text) throw tee
     const apiUrll = `https://api.lolhuman.xyz/api/textprome/jokerlogo?apikey=${lolkeysapi}&text=${encodeURIComponent(text)}`;
     conn.sendFile(m.chat, apiUrll, 'logo.png', '✅ Result', m);
-    m.react('✅');
+    ('✅');
     break;
 
 	case 'gfx6': 
@@ -50,7 +50,7 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
 	let [c, d] = text.split`+`   
 	const apiUrl = `https://api.lolhuman.xyz/api/textprome2/lionlogo?apikey=${lolkeysapi}&text1=${encodeURIComponent(c)}&text2=${encodeURIComponent(d)}`
 	conn.sendFile(m.chat, apiUrl, 'logo.png', `✅ Result`, m)
-	m.react(done)
+	(done)
 	break 
 	case 'gfx7': 
 	if (!text) throw too;
@@ -59,25 +59,25 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
 	let oda = `https://api.lolhuman.xyz/api/photooxy2/battlefield4?apikey=${lolkeysapi}&text1=${encodeURIComponent(e.trim())}&text2=${encodeURIComponent(f.trim())}`;
 	conn.sendFile(m.chat, oda, 'logo.png', `✅ Result`, m);
 	
-	m.react(done);
+	(done);
 	break 
 	case 'gfx8': 
 	if (!text) throw tee;
 	let rand = `https://api.lolhuman.xyz/api/ephoto1/anonymhacker?apikey=${lolkeysapi}&text=${encodeURIComponent(text)}`;
 	conn.sendFile(m.chat, rand, 'logo.png', `✅ Result`, m);
-	m.react(done);
+	(done);
 	break;
 	case 'gfx9': 
 	if (!text) throw tee;
 	let randi = `https://api.lolhuman.xyz/api/ephoto1/avatarlolnew?apikey=${lolkeysapi}&text=${encodeURIComponent(text)}`;
 	conn.sendFile(m.chat, randi, 'logo.png', `✅ Result`, m);
-	m.react(done);
+	(done);
 	break;
 	case 'gfx10': 
 	if (!text) throw tee;
 	let randu = `https://api.lolhuman.xyz/api/ephoto1/avatardota?apikey=${lolkeysapi}&text=${encodeURIComponent(text)}`;
 	conn.sendFile(m.chat, randu, 'logo.png', `✅ Result`, m);
-	m.react(done);
+	(done);
 	break;
 	case 'gfx11': 
 	if (!text) throw too;
@@ -86,13 +86,13 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
 	let od = `https://api.lolhuman.xyz/api/ephoto2/codwarzone?apikey=${lolkeysapi}&text1=${encodeURIComponent(g.trim())}&text2=${encodeURIComponent(h.trim())}`;
 	conn.sendFile(m.chat, od, 'logo.png', `✅ Result`, m);
 	
-	m.react(done);
+	(done);
 	break 
 	case 'gfx12': 
 	if (!text) throw tee;
 	let rr = `https://api.lolhuman.xyz/api/ephoto1/freefire?apikey=${lolkeysapi}&text=${encodeURIComponent(text)}`;
 	conn.sendFile(m.chat, rr, 'logo.png', `✅ Result`, m);
-	m.react(done);
+	(done);
 	break;
 
 	  default:
@@ -104,4 +104,3 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
   handler.command = /^(gfx1|gfx2|gfx3|gfx4|gfx5|gfx6|gfx7|gfx8|gfx9|gfx10|gfx11|gfx12)$/i;
   handler.diamond = false;
   
-  export default handler;
