813cadad25dda5585a49daea94a354a4d17b0876
𝑴𝒐𝒏𝒌𝒆𝒚-𝑫-𝑳𝒐𝒇𝒇𝒚١⁵ <155516056+ze819@users.noreply.github.com>
2024-03-26 08:25:45 +0300

    Update afk-_afk.js

--- plugins/afk-_afk.js
+++ plugins/afk-_afk.js
@@ -1,15 +1,11 @@
-
-
-
-
 let handler = async (m, { conn }) => {
     let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-    conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/misc/horny', {
-    avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/511e2eb56675df0219c3d.jpg'),
-    }), 'hornycard.png', '*🌸𝑀𝐴𝐾𝐼𝑀𝐴-𝐵𝛩𝑇🌸*',fkontak ,m)
+    conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/misc/lolice', {
+    avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
+    }), 'error.png', '🌸*𝑀𝐴𝐾𝐼𝑀𝐴-𝐵𝛩𝑇*🌸',fkontak ,m)
     }
-    handler.help = ['hornycard', 'hornylicense']
-    handler.tags = ['maker'] 
-    handler.command = /^(بروفايل3|بروفايلي3)$/i 
+    handler.help = ['lolicon']
+    handler.tags = ['maker']
+    handler.command = /^(بروفايل4)$/i
     export default handler
