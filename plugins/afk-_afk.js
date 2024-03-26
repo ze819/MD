a25814578b666389b4eed905b97c992de00353dc
𝑴𝒐𝒏𝒌𝒆𝒚-𝑫-𝑳𝒐𝒇𝒇𝒚١⁵ <155516056+ze819@users.noreply.github.com>
2024-03-26 07:49:02 +0300

    Update afk-_afk.js

--- plugins/afk-_afk.js
+++ plugins/afk-_afk.js
@@ -1,15 +1,15 @@
-let handler = async (m, { conn }) => {
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-    let name = conn.getName(who)
-    let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './Guru.jpg')
-    conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/overlay/wasted', {
-      avatar: pp, 
-    }), 'waste.png', `*تم يا ${name}*\n\n*𝐋𝐔𝐅𝐅𝐘-𝐁𝐎𝐓*`,fkontak ,m)
-  }
-  
-  handler.help = ['waste @user']
-  handler.tags = ['fun']
-  handler.command = ['بروفايل1'] 
+
   
   export default handler
+
+let handler = async (m, { conn, usedprefix }) => {
+    let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+    conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/misc/blur', {
+    avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
+    }), 'hornycard.png', '*🌸𝑀𝐴𝐾𝐼𝑀𝐴-𝐵𝛩𝑇🌸*'fkontak, ,m)
+    }
+    handler.help = ['blur','difuminar2']
+    handler.tags = ['maker']
+    handler.command = /^(بروفايلي2|بروفايل2)$/i
+    export default handler
