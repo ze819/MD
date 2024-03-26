e52e47b50f8acc63460c642940a91c40d9bd430d
𝑴𝒐𝒏𝒌𝒆𝒚-𝑫-𝑳𝒐𝒇𝒇𝒚١⁵ <155516056+ze819@users.noreply.github.com>
2024-03-26 10:51:35 +0300

    Update afk-_afk.js

--- plugins/afk-_afk.js
+++ plugins/afk-_afk.js
@@ -11,7 +11,7 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
 		if (!text) throw tee;
 		let chut = `https://api.caliph.biz.id/api/kaneki?nama=${encodeURIComponent(text)}&apikey=caliphkey`;
 		conn.sendFile(m.chat, chut, 'logo.png', `✅ Result`, m);
-		(done);
+		
 		break;
 		
 	  case 'gfx2': 
@@ -21,21 +21,21 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
 		let loda = `https://api.caliph.biz.id/api/girlneko?nama=${encodeURIComponent(a.trim())}&nama2=${encodeURIComponent(b.trim())}&apikey=caliphkey`;
 		conn.sendFile(m.chat, loda, 'logo.png', `✅ Result`, m);
 		
-		(done);
+		
 		break;
 		
 	  case 'gfx3':
 		if (!text) throw tee;
 		let cp = `https://api.caliph.biz.id/api/rem?nama=${encodeURIComponent(text.trim())}&apikey=caliphkey`;
 		conn.sendFile(m.chat, cp, 'logo.png', `✅ Result`, m);
-		(done);
+		
 		break;
 		
 	  case 'gfx4': 
 		if (!text) throw tee;
 		let gandu = `https://api.caliph.biz.id/api/textpro/matrix?text=${encodeURIComponent(text)}&apikey=caliphkey`;
 		conn.sendFile(m.chat, gandu, 'logo.png', `✅ Result`, m);
-		(done);
+		
 		break;
 		case 'gfx5':
     if (!text) throw tee
@@ -59,25 +59,25 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
 	let oda = `https://api.lolhuman.xyz/api/photooxy2/battlefield4?apikey=${lolkeysapi}&text1=${encodeURIComponent(e.trim())}&text2=${encodeURIComponent(f.trim())}`;
 	conn.sendFile(m.chat, oda, 'logo.png', `✅ Result`, m);
 	
-	(done);
+	
 	break 
 	case 'gfx8': 
 	if (!text) throw tee;
 	let rand = `https://api.lolhuman.xyz/api/ephoto1/anonymhacker?apikey=${lolkeysapi}&text=${encodeURIComponent(text)}`;
 	conn.sendFile(m.chat, rand, 'logo.png', `✅ Result`, m);
-	(done);
+	
 	break;
 	case 'gfx9': 
 	if (!text) throw tee;
 	let randi = `https://api.lolhuman.xyz/api/ephoto1/avatarlolnew?apikey=${lolkeysapi}&text=${encodeURIComponent(text)}`;
 	conn.sendFile(m.chat, randi, 'logo.png', `✅ Result`, m);
-	(done);
+	
 	break;
 	case 'gfx10': 
 	if (!text) throw tee;
 	let randu = `https://api.lolhuman.xyz/api/ephoto1/avatardota?apikey=${lolkeysapi}&text=${encodeURIComponent(text)}`;
 	conn.sendFile(m.chat, randu, 'logo.png', `✅ Result`, m);
-	(done);
+	
 	break;
 	case 'gfx11': 
 	if (!text) throw too;
@@ -86,13 +86,13 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
 	let od = `https://api.lolhuman.xyz/api/ephoto2/codwarzone?apikey=${lolkeysapi}&text1=${encodeURIComponent(g.trim())}&text2=${encodeURIComponent(h.trim())}`;
 	conn.sendFile(m.chat, od, 'logo.png', `✅ Result`, m);
 	
-	(done);
+	
 	break 
 	case 'gfx12': 
 	if (!text) throw tee;
 	let rr = `https://api.lolhuman.xyz/api/ephoto1/freefire?apikey=${lolkeysapi}&text=${encodeURIComponent(text)}`;
 	conn.sendFile(m.chat, rr, 'logo.png', `✅ Result`, m);
-	(done);
+	
 	break;
 
 	  default:
