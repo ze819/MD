7b097f2ba0a32945049ee9817e6417ff101280e8
𝑴𝒐𝒏𝒌𝒆𝒚-𝑫-𝑳𝒐𝒇𝒇𝒚١⁵ <155516056+ze819@users.noreply.github.com>
2024-03-26 08:56:40 +0300

    Update afk-_afk.js

--- plugins/afk-_afk.js
+++ plugins/afk-_afk.js
@@ -1,13 +1,21 @@
-let handler = async (m, { conn, args }) => {
+let handler = async (m, { conn, text }) => {
     let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-    let text = args.slice(1).join(' ')
-    let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-    conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/misc/its-so-stupid', {
-    avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/2d2899be76e41d860a68b.jpg'),
-    dog: text || 'im+stupid'
-    }), 'error.png', `*𝐿𝑈𝐹𝐹𝑌-𝐵𝛩𝑇*`,fkontak ,m)
-    }  
-    handler.help = ['itssostupid', 'iss', 'stupid']
-    handler.tags = ['maker']
-    handler.command = /^(بروفايلي6|ب6|بروفايل6)$/i
-    export default handler
+    if (!text) throw 'No Text';
+  
+    const avatar = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/2d2899be76e41d860a68b.jpg');
+    const displayName = conn.getName(m.sender);
+    const username = m.sender.split('@')[0];
+    const replies = '69'; // Replace with the desired value
+    const retweets = '69'; // Replace with the desired value
+    const theme = 'dark'; // Replace with the desired value
+  
+    const url = `https://some-random-api.com/canvas/misc/tweet?displayname=${encodeURIComponent(displayName)}&username=${encodeURIComponent(username)}&avatar=${encodeURIComponent(avatar)}&comment=${encodeURIComponent(text)}&replies=${encodeURIComponent(replies)}&retweets=${encodeURIComponent(retweets)}&theme=${encodeURIComponent(theme)}`;
+  
+    conn.sendFile(m.chat, url, 'tweet.png', '*𝐿𝑈𝐹𝐹𝑌-𝐵𝛩𝑇*',fkontak ,m);
+  };
+  
+  handler.help = ['tweet <comment>'];
+  handler.tags = ['maker'];
+  handler.command = /^(تويت|ب7|بروفايل7)$/i;
+  
+  export default handler;
