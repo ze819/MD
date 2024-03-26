b76b6282b6fbb98c9ca10eafbc37ef5d21f6213b
𝑴𝒐𝒏𝒌𝒆𝒚-𝑫-𝑳𝒐𝒇𝒇𝒚١⁵ <155516056+ze819@users.noreply.github.com>
2024-03-26 10:24:24 +0300

    Update afk-_afk.js

--- plugins/afk-_afk.js
+++ plugins/afk-_afk.js
@@ -1,13 +1,15 @@
-let handler = async (m, { conn, text }) => {
-    let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-    if (!text) throw '*نسيت النص يرجي كتابه نص او اسمك*'
-    conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/misc/youtube-comment', {
-    avatar: await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/2d2899be76e41d860a68b.jpg'),
-    comment: text,
-    username: conn.getName(m.sender)
-    }), 'error.png', '*ا𝐿𝑈𝐹𝐹𝑌-𝐵𝛩𝑇*\n*مـــطـــوري*\n*967733707084*',fkontak ,m)
-    }
-    handler.help = ['ytcomment <comment>']
-    handler.tags = ['maker'] 
-    handler.command = /^(تويت2|ب8|بروفايل8|بروفايلي8)$/i
-    export default handler
+let handler = async (m, { conn }) => {
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+  let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+  let name = conn.getName(who)
+  let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './Guru.jpg')
+  conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/overlay/gay', {
+    avatar: pp, 
+  }), 'gay.png', `*هل انت 🏳‍🌈 يا${name}\n\n*🌸𝑀𝐴𝐾𝐼𝑀𝐴-𝐵𝛩𝑇🌸*`,fkontak ,m)
+}
+
+handler.help = ['gay @user']
+handler.tags = ['fun']
+handler.command = ['ب8','بروفايلي8','بروفايل8'] 
+
+export default handler
