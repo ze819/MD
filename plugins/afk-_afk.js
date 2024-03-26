7538668a9b467176ca58ab029726a73cc58cef71
𝑴𝒐𝒏𝒌𝒆𝒚-𝑫-𝑳𝒐𝒇𝒇𝒚١⁵ <155516056+ze819@users.noreply.github.com>
2024-03-26 08:46:22 +0300

    Update afk-_afk.js

--- plugins/afk-_afk.js
+++ plugins/afk-_afk.js
@@ -1,11 +1,13 @@
-let handler = async (m, { conn }) => {
+let handler = async (m, { conn, args }) => {
     let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+    let text = args.slice(1).join(' ')
     let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
-    conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/misc/simpcard', {
-    avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/2d2899be76e41d860a68b.jpg'),
-    }), 'error.png', '*🌸𝑀𝐴𝐾𝐼𝑀𝐴-𝐵𝛩𝑇🌸*',fkontak ,m)
-    }
-    handler.help = ['simpcard']
-    handler.tags = ['maker']  
-    handler.command = /^(بروفايل5|بروفايلي5)$/i  
+    conn.sendFile(m.chat, global.API('https://some-random-api.com', '/canvas/misc/its-so-stupid', {
+    avatar: await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png'),
+    dog: text || 'im+stupid'
+    }), 'error.png', `*@${author}*`,fkontak ,m)
+    }  
+    handler.help = ['itssostupid', 'iss', 'stupid']
+    handler.tags = ['maker']
+    handler.command = /^(بروفايلي6|ب6|بروفايل6)$/i
     export default handler
