928dbfba131cca02700288eb688bfac1579f1ceb
𝑴𝒐𝒏𝒌𝒆𝒚-𝑫-𝑳𝒐𝒇𝒇𝒚١⁵ <155516056+ze819@users.noreply.github.com>
2024-03-27 16:30:32 +0300

    Update afk-_afk.js

--- plugins/afk-_afk.js
+++ plugins/afk-_afk.js
@@ -1,7 +1,7 @@
         let handler = async (m, { conn, args, text, usedPrefix, command }) => {
 	let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 	let tee = `*🍁𝐌𝐎𝐍𝐊𝐄𝐘 𝐃 𝐋𝐔𝐅𝐅𝐘١⁵🍁*\n\n*مثــال:* *${usedPrefix + command}* *LUFFY*\n*بعض منها تدعم اللغه العربيه*`
-	let too = `*🍁𝐌𝐎𝐍𝐊𝐄𝐘 𝐃 𝐋𝐔𝐅𝐅𝐘١⁵🍁*\n\nمثال:*\n* *${usedPrefix + command}* *LUFFY* *+* *BOT*`
+	let too = `*🍁𝐌𝐎𝐍𝐊𝐄𝐘 𝐃 𝐋𝐔𝐅𝐅𝐘١⁵🍁*\n\n*مـثــال:*\n* *${usedPrefix + command}* *LUFFY* *+* *BOT*`
        
 	
 	
@@ -11,8 +11,8 @@
 	  case 'ل1':
 		if (!text) throw tee;
 		let chut = `https://api.caliph.biz.id/api/kaneki?nama=${encodeURIComponent(text)}&apikey=caliphkey`;
-		await conn.reply(m.chat, '*انتضر جار التنفيذ..*', fkontak, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: '𝐿𝑈𝐹𝐹𝑌-𝐵𝛩𝑇', body: `【𝟐𝟎𝟎𝟓/𝟐/𝟏𝟎】١⁵`, mediaType: 2, sourceUrl: md, thumbnail: imagen1}}}, { quoted: m })
-		conn.sendFile(m.chat, chut, 'logo.png', `*🍁𝐌𝐎𝐍𝐊𝐄𝐘 𝐃 𝐋𝐔𝐅𝐅𝐘١⁵🍁*`, m);
+		await conn.reply(m.chat, '*انتضر جار التنفيذ..*', fkontak, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: '♻️انضم الى قناتي♻️', body: `𝐿𝑈𝐹𝐹𝑌-𝐵𝛩𝑇`, mediaType: 2, sourceUrl: nnn, thumbnail: imagen1}}}, { quoted: m })
+		conn.sendFile(m.chat, chut, 'logo.png', `*🍁𝐌𝐎𝐍𝐊𝐄𝐘 𝐃 𝐋𝐔𝐅𝐅𝐘١⁵🍁*`,fkontak ,m);
 		
 		break;
 		
@@ -21,7 +21,8 @@
 		if (!text.includes('+')) throw too;
 		let [a, b] = text.split('+');
 		let loda = `https://api.caliph.biz.id/api/girlneko?nama=${encodeURIComponent(a.trim())}&nama2=${encodeURIComponent(b.trim())}&apikey=caliphkey`;
-		conn.sendFile(m.chat, loda, 'logo.png', `*🍁𝐌𝐎𝐍𝐊𝐄𝐘 𝐃 𝐋𝐔𝐅𝐅𝐘١⁵🍁*`, m);
+		await conn.reply(m.chat, '*انتضر جار التنفيذ..*', fkontak, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: '♻️انضم الى قناتي♻️', body: `𝐿𝑈𝐹𝐹𝑌-𝐵𝛩𝑇`, mediaType: 2, sourceUrl: nnn, thumbnail: imagen1}}}, { quoted: m })	
+		conn.sendFile(m.chat, loda, 'logo.png', `*🍁𝐌𝐎𝐍𝐊𝐄𝐘 𝐃 𝐋𝐔𝐅𝐅𝐘١⁵🍁*`,fkontak ,m);
 		
 		
 		break;
@@ -29,20 +30,23 @@
 	  case 'ل3':
 		if (!text) throw tee;
 		let cp = `https://api.caliph.biz.id/api/rem?nama=${encodeURIComponent(text.trim())}&apikey=caliphkey`;
-		conn.sendFile(m.chat, cp, 'logo.png', `*🍁𝐌𝐎𝐍𝐊𝐄𝐘 𝐃 𝐋𝐔𝐅𝐅𝐘١⁵🍁*`, m);
+		await conn.reply(m.chat, '*انتضر جار التنفيذ..*', fkontak, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: '♻️انضم الى قناتي♻️', body: `𝐿𝑈𝐹𝐹𝑌-𝐵𝛩𝑇`, mediaType: 2, sourceUrl: nnn, thumbnail: imagen1}}}, { quoted: m })	
+		conn.sendFile(m.chat, cp, 'logo.png', `*🍁𝐌𝐎𝐍𝐊𝐄𝐘 𝐃 𝐋𝐔𝐅𝐅𝐘١⁵🍁*`,fkontak ,m);
 		
 		break;
 		
 	  case 'لxxx': 
 		if (!text) throw tee;
 		let gandu = `https://api.caliph.biz.id/api/textpro/matrix?text=${encodeURIComponent(text)}&apikey=caliphkey`;
-		conn.sendFile(m.chat, gandu, 'logo.png', `*🍁𝐌𝐎𝐍𝐊𝐄𝐘 𝐃 𝐋𝐔𝐅𝐅𝐘١⁵🍁*`, m);
+		await conn.reply(m.chat, '*انتضر جار التنفيذ..*', fkontak, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: '♻️انضم الى قناتي♻️', body: `𝐿𝑈𝐹𝐹𝑌-𝐵𝛩𝑇`, mediaType: 2, sourceUrl: nnn, thumbnail: imagen1}}}, { quoted: m })	
+		conn.sendFile(m.chat, gandu, 'logo.png', `*🍁𝐌𝐎𝐍𝐊𝐄𝐘 𝐃 𝐋𝐔𝐅𝐅𝐘١⁵🍁*`,fkontak ,m);
 		
 		break;
 		case 'xxل':
     if (!text) throw tee
     const apiUrll = `https://api.lolhuman.xyz/api/textprome/jokerlogo?apikey=${lolkeysapi}&text=${encodeURIComponent(text)}`;
-    conn.sendFile(m.chat, apiUrll, 'logo.png', '*🍁𝐌𝐎𝐍𝐊𝐄𝐘 𝐃 𝐋𝐔𝐅𝐅𝐘١⁵🍁*', m);
+    await conn.reply(m.chat, '*انتضر جار التنفيذ..*', fkontak, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: '♻️انضم الى قناتي♻️', body: `𝐿𝑈𝐹𝐹𝑌-𝐵𝛩𝑇`, mediaType: 2, sourceUrl: nnn, thumbnail: imagen1}}}, { quoted: m })	
+    conn.sendFile(m.chat, apiUrll, 'logo.png', '*🍁𝐌𝐎𝐍𝐊𝐄𝐘 𝐃 𝐋𝐔𝐅𝐅𝐘١⁵🍁*',fkontak ,m);
     
     break;
 
@@ -51,7 +55,8 @@
 	if (!text.includes('+')) throw too  
 	let [c, d] = text.split`+`   
 	const apiUrl = `https://api.lolhuman.xyz/api/textprome2/lionlogo?apikey=${lolkeysapi}&text1=${encodeURIComponent(c)}&text2=${encodeURIComponent(d)}`
-	conn.sendFile(m.chat, apiUrl, 'logo.png', `*🍁𝐌𝐎𝐍𝐊𝐄𝐘 𝐃 𝐋𝐔𝐅𝐅𝐘١⁵🍁*`, m)
+	await conn.reply(m.chat, '*انتضر جار التنفيذ..*', fkontak, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: '♻️انضم الى قناتي♻️', body: `𝐿𝑈𝐹𝐹𝑌-𝐵𝛩𝑇`, mediaType: 2, sourceUrl: nnn, thumbnail: imagen1}}}, { quoted: m })	
+	conn.sendFile(m.chat, apiUrl, 'logo.png', `*🍁𝐌𝐎𝐍𝐊𝐄𝐘 𝐃 𝐋𝐔𝐅𝐅𝐘١⁵🍁*`,fkontak ,m)
 	
 	break 
 	case 'ل4': 
@@ -59,26 +64,30 @@
 	if (!text.includes('+')) throw too;
 	let [e, f] = text.split('+');
 	let oda = `https://api.lolhuman.xyz/api/photooxy2/battlefield4?apikey=${lolkeysapi}&text1=${encodeURIComponent(e.trim())}&text2=${encodeURIComponent(f.trim())}`;
-	conn.sendFile(m.chat, oda, 'logo.png', `*🍁𝐌𝐎𝐍𝐊𝐄𝐘 𝐃 𝐋𝐔𝐅𝐅𝐘١⁵🍁*`, m);
+	await conn.reply(m.chat, '*انتضر جار التنفيذ..*', fkontak, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: '♻️انضم الى قناتي♻️', body: `𝐿𝑈𝐹𝐹𝑌-𝐵𝛩𝑇`, mediaType: 2, sourceUrl: nnn, thumbnail: imagen1}}}, { quoted: m })	
+	conn.sendFile(m.chat, oda, 'logo.png', `*🍁𝐌𝐎𝐍𝐊𝐄𝐘 𝐃 𝐋𝐔𝐅𝐅𝐘١⁵🍁*`,fkontak ,m);
 	
 	
 	break 
 	case 'ل5': 
 	if (!text) throw tee;
 	let rand = `https://api.lolhuman.xyz/api/ephoto1/anonymhacker?apikey=${lolkeysapi}&text=${encodeURIComponent(text)}`;
-	conn.sendFile(m.chat, rand, 'logo.png', `*🍁𝐌𝐎𝐍𝐊𝐄𝐘 𝐃 𝐋𝐔𝐅𝐅𝐘١⁵🍁*`, m);
+	await conn.reply(m.chat, '*انتضر جار التنفيذ..*', fkontak, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: '♻️انضم الى قناتي♻️', body: `𝐿𝑈𝐹𝐹𝑌-𝐵𝛩𝑇`, mediaType: 2, sourceUrl: nnn, thumbnail: imagen1}}}, { quoted: m })	
+	conn.sendFile(m.chat, rand, 'logo.png', `*🍁𝐌𝐎𝐍𝐊𝐄𝐘 𝐃 𝐋𝐔𝐅𝐅𝐘١⁵🍁*`,fkontak ,m);
 	
 	break;
 	case 'ل6': 
 	if (!text) throw tee;
 	let randi = `https://api.lolhuman.xyz/api/ephoto1/avatarlolnew?apikey=${lolkeysapi}&text=${encodeURIComponent(text)}`;
-	conn.sendFile(m.chat, randi, 'logo.png', `*🍁𝐌𝐎𝐍𝐊𝐄𝐘 𝐃 𝐋𝐔𝐅𝐅𝐘١⁵🍁*`, m);
+	await conn.reply(m.chat, '*انتضر جار التنفيذ..*', fkontak, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: '♻️انضم الى قناتي♻️', body: `𝐿𝑈𝐹𝐹𝑌-𝐵𝛩𝑇`, mediaType: 2, sourceUrl: nnn, thumbnail: imagen1}}}, { quoted: m })	
+	conn.sendFile(m.chat, randi, 'logo.png', `*🍁𝐌𝐎𝐍𝐊𝐄𝐘 𝐃 𝐋𝐔𝐅𝐅𝐘١⁵🍁*`,fkontak ,m);
 	
 	break;
 	case 'ل7': 
 	if (!text) throw tee;
 	let randu = `https://api.lolhuman.xyz/api/ephoto1/avatardota?apikey=${lolkeysapi}&text=${encodeURIComponent(text)}`;
-	conn.sendFile(m.chat, randu, 'logo.png', `*🍁𝐌𝐎𝐍𝐊𝐄𝐘 𝐃 𝐋𝐔𝐅𝐅𝐘١⁵🍁*`, m);
+	await conn.reply(m.chat, '*انتضر جار التنفيذ..*', fkontak, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: '♻️انضم الى قناتي♻️', body: `𝐿𝑈𝐹𝐹𝑌-𝐵𝛩𝑇`, mediaType: 2, sourceUrl: nnn, thumbnail: imagen1}}}, { quoted: m })	
+	conn.sendFile(m.chat, randu, 'logo.png', `*🍁𝐌𝐎𝐍𝐊𝐄𝐘 𝐃 𝐋𝐔𝐅𝐅𝐘١⁵🍁*`,fkontak ,m);
 	
 	break;
 	case 'ل8': 
@@ -86,14 +95,16 @@
 	if (!text.includes('+')) throw too;
 	let [g, h] = text.split('+');
 	let od = `https://api.lolhuman.xyz/api/ephoto2/codwarzone?apikey=${lolkeysapi}&text1=${encodeURIComponent(g.trim())}&text2=${encodeURIComponent(h.trim())}`;
-	conn.sendFile(m.chat, od, 'logo.png', `*🍁𝐌𝐎𝐍𝐊𝐄𝐘 𝐃 𝐋𝐔𝐅𝐅𝐘١⁵🍁*`, m);
+	await conn.reply(m.chat, '*انتضر جار التنفيذ..*', fkontak, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: '♻️انضم الى قناتي♻️', body: `𝐿𝑈𝐹𝐹𝑌-𝐵𝛩𝑇`, mediaType: 2, sourceUrl: nnn, thumbnail: imagen1}}}, { quoted: m })	
+	conn.sendFile(m.chat, od, 'logo.png', `*🍁𝐌𝐎𝐍𝐊𝐄𝐘 𝐃 𝐋𝐔𝐅𝐅𝐘١⁵🍁*`,fkontak ,m);
 	
 	
 	break 
 	case 'ل9': 
 	if (!text) throw tee;
 	let rr = `https://api.lolhuman.xyz/api/ephoto1/freefire?apikey=${lolkeysapi}&text=${encodeURIComponent(text)}`;
-	conn.sendFile(m.chat, rr, 'logo.png', `*🍁𝐌𝐎𝐍𝐊𝐄𝐘 𝐃 𝐋𝐔𝐅𝐅𝐘١⁵🍁*`, m);
+	await conn.reply(m.chat, '*انتضر جار التنفيذ..*', fkontak, { contextInfo:{ forwardingScore: 2022, isForwarded: true, externalAdReply: {title: '♻️انضم الى قناتي♻️', body: `𝐿𝑈𝐹𝐹𝑌-𝐵𝛩𝑇`, mediaType: 2, sourceUrl: nnn, thumbnail: imagen1}}}, { quoted: m })	
+	conn.sendFile(m.chat, rr, 'logo.png', `*🍁𝐌𝐎𝐍𝐊𝐄𝐘 𝐃 𝐋𝐔𝐅𝐅𝐘١⁵🍁*`,fkontak ,m);
 	break;
 
 	  default:
